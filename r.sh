9437f66796c5673933d4a94b3946a9b58afc702b
duytuan207 <88073690+duytuan207@users.noreply.github.com>
2023-09-14 11:52:09 +0700

    Create r.sh

---
+++ r.sh
@@ -0,0 +1,13 @@
+//r.sh
+//this source belong to Nguyen Duy Tuan, tks!
+//say hi to everyone see this source code...
+pm2 delete all
+
+
+
+
+clear
+pm2 start "npm run start"
+//pm2 monit
+pm2 logs
+//node cleanup 
